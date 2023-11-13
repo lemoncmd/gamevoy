d7cc4e355a7c70ee340b25bfd49f1098c02c5925
lemoncmd <lemoncmd@gmail.com>
2023-11-14 01:02:33 +0900

    implement mmio

--- src/peripherals/bootrom/bootrom.v
+++ src/peripherals/bootrom/bootrom.v
@@ -4,12 +4,22 @@ const size = 0x100
 
 pub struct BootRom {
 	rom [size]u8
+mut:
+	active bool
 }
 
 pub fn BootRom.new(rom [size]u8) BootRom {
-	return BootRom{rom}
+	return BootRom{rom, true}
 }
 
 pub fn (b BootRom) read(addr u16) u8 {
 	return b.rom[addr]
+}
+
+pub fn (mut b BootRom) write(addr u16, val u8) {
+	b.active = b.active && val == 0
+}
+
+pub fn (b BootRom) is_active() bool {
+	return b.active
 }
--- src/peripherals/hram/hram.v
+++ src/peripherals/hram/hram.v
@@ -12,9 +12,9 @@ pub fn HRam.new() HRam {
 }
 
 pub fn (h HRam) read(addr u16) u8 {
-	return h.ram[addr]
+	return h.ram[addr & (size - 1)]
 }
 
 pub fn (mut h HRam) write(addr u16, val u8) {
-	h.ram[addr] = val
+	h.ram[addr & (size - 1)] = val
 }
